3594c936b25b5511dd277a34839b141d2cad3cd4
So00 <anthony.tournerie@gmail.com>
2023-10-01 22:44:45 +0200

    feat: add ProjectionClass to export

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
+export * from "./application-events";
+export * from "./event-sourcing.module";
+export * from "./eventstore.provider";
 export * from "./eventstore";
+export * from "./projection-classes";
 export * from "./store-event-bus";
 export * from "./store-event-publisher";
-export * from "./eventstore.provider";
-export * from "./event-sourcing.module";
-export * from "./application-events";
+
 export * from "./entities/event";
+
 export * from "./exception/event-not-exist.exception";
 export * from "./exception/base-exception";
+
 export * from "./interface/event-sourcing";
 export * from "./interface/projection";
+export * from "./interface/projection-class";
 export * from "./interface/root-async-options";
 export * from "./interface/storable-event";
